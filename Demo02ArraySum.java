d8e68bfd0f81114e06f1d72d5a75d732bb2766c4
Love-H <ssyx.hj@gmail.com>
2018-10-08 21:11:56 +0800

    day04

---
+++ Demo02ArraySum.java
@@ -0,0 +1,14 @@
+package day04.demo;
+
+//求数组元素和
+
+public class Demo02ArraySum {
+	public static void main(String[] args) {
+		int[] Array1 = {12,74,98,63,45,82};
+		int sum = 0;
+		for(int i = 0;i < Array1.length;i++) {
+			sum += Array1[i]; 
+		}
+		System.out.println("该数组和为:"+sum);
+	}
+}
